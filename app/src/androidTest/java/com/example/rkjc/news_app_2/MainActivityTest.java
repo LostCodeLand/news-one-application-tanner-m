d90e3920178311bcf61812203aacfbd77b571f04
mesargent <markesargent@gmail.com>
2018-10-28 18:20:03 -0700

    Network call returns usable JSON test completed

--- app/src/androidTest/java/com/example/rkjc/news_app_2/MainActivityTest.java
+++ app/src/androidTest/java/com/example/rkjc/news_app_2/MainActivityTest.java
@@ -2,8 +2,11 @@ package com.example.rkjc.news_app_2;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -15,12 +18,18 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+@LargeTest
+public class MainActivityTest {
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule =
+            new ActivityTestRule(MainActivity.class);
+
+
+
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.rkjc.news_app_2", appContext.getPackageName());
     }
 }
